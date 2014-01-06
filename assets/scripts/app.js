e5b6863a650193fe38bfc92f9f90e62c3900142b
Matt O'Keefe <mnokeefe@gmail.com>
2014-01-06 17:32:39 +0000

    App bar expanding in progress
    Still to do:
    Even spacing and support for longer titles
    Limit to 5 icons and then list further items

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,12 +1,27 @@
+/* ------- */
+/* App bar */
+/* ------- */
+
+// Grap contextual icons on load
+$(".ui-panorama-item-active .js-icon-added").clone().prependTo(".app-bar__items");
+
 // Get active panel and update app bar on swipe
 $('.ui-panorama').on('selectedItemChanged', function(){
   $('.ui-panorama-item-active').removeClass('ui-panorama-item-active')
   $('.ui-panorama').data('obj').getItem($('.ui-panorama').data('obj').opts.currentIndex).addClass('ui-panorama-item-active');
   $(".js-app-bar .js-icon-added").remove();
-  $(".ui-panorama-item-active .js-icon-added").clone().appendTo(".js-app-bar");
-})
+  $(".ui-panorama-item-active .js-icon-added").clone().prependTo(".app-bar__items");
+});
 
-$(".js-static-app-bar-icons .js-icon-added").clone().appendTo(".js-app-bar"); // For single pages
+
+// Expand and collapse
+$(".js-app-bar-expand").click(function(){
+  if($(".app-bar").hasClass("js-app-bar-collapsed")) {
+    $(".app-bar").animate({height:54},200).removeClass("js-app-bar-collapsed");
+  } else { 
+    $(".app-bar").animate({height:80},200).addClass("js-app-bar-collapsed");
+  }
+});
 
 // Keybinding for desktop
 $(document).on("keydown", function (e) {
