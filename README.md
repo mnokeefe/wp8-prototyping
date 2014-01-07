a4fcf98013b535e4405dfc26c69ed9b1a8e51ee3
Matt O'Keefe <mnokeefe@gmail.com>
2014-01-07 17:28:29 +0000

    App bar expanding done, needs tidying
    Working on bloom specific pages

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Windows 8 Prototyping tool
+
+## App bar
+
+From Microsoft's guidelines:
+
+* Avoid using long text for menu items because the text will run off the screen. The recommended maximum length is between 14 and 20 characters.
+* Avoid using more than five menu items on an Application Bar because it will force the user to scroll.
+
+## Panorama
+
+## Pivot
+
--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,27 +1,67 @@
-/* ------- */
-/* App bar */
-/* ------- */
+/* ---------------------------- */
+/* App bar                      */
+/* ---------------------------- */
 
-// Grap contextual icons on load
-$(".ui-panorama-item-active .js-icon-added").clone().prependTo(".app-bar__items");
+var addedAppBarItems = [];
+var appBarHeight = 54;
+
+function updateAppBar() {
+
+  $(".js-app-bar .js-icon-added").remove();
+  $(".app-bar__extra-items").empty();
+
+  // Grab the hidden app bar icons for the active tab
+  addedAppBarItems = [];
+  $('.ui-panorama-item-active .js-app-bar-icons li').each( function() {
+    addedAppBarItems.push($(this).html());
+  });
+
+  var numberAddedItems = addedAppBarItems.length;
+
+  // Clone them in to the app bar
+  if (numberAddedItems < 4) {
+    for (var i = 0; i < numberAddedItems; i++) {
+      $(".app-bar__items").prepend('<li class="app-bar__item js-icon-added">' + addedAppBarItems[i] + '</li>');
+    }
+  } else {
+    for (var i = 0; i < 4; i++) {
+      $(".app-bar__items").prepend('<li class="app-bar__item js-icon-added">' + addedAppBarItems[i] + '</li>');
+    }
+    for (var i = 4; i < numberAddedItems; i++) {
+      $(".app-bar__extra-items").prepend(addedAppBarItems[i]);
+    }
+  }
+};
+updateAppBar(); // Update it on load as well as swipe
 
 // Get active panel and update app bar on swipe
 $('.ui-panorama').on('selectedItemChanged', function(){
+
   $('.ui-panorama-item-active').removeClass('ui-panorama-item-active')
   $('.ui-panorama').data('obj').getItem($('.ui-panorama').data('obj').opts.currentIndex).addClass('ui-panorama-item-active');
-  $(".js-app-bar .js-icon-added").remove();
-  $(".ui-panorama-item-active .js-icon-added").clone().prependTo(".app-bar__items");
-});
 
+  // Update the app bar
+  updateAppBar();
+});
 
 // Expand and collapse
 $(".js-app-bar-expand").click(function(){
-  if($(".app-bar").hasClass("js-app-bar-collapsed")) {
-    $(".app-bar").animate({height:54},200).removeClass("js-app-bar-collapsed");
-  } else { 
-    $(".app-bar").animate({height:80},200).addClass("js-app-bar-collapsed");
-  }
+  if($(".app-bar").hasClass("js-app-bar-expanded")) {
+    $(".app-bar").animate({height: 54}, 200).removeClass("js-app-bar-expanded");
+  } else {
+      if (addedAppBarItems.length > 4) {
+        appBarHeight = 80 + (addedAppBarItems.length - 4)*60;
+        $(".app-bar").animate({height: appBarHeight}, 200).addClass("js-app-bar-expanded");
+      } else {
+      $(".app-bar").animate({height: 80}, 200).addClass("js-app-bar-expanded");
+    };
+  };
 });
+
+/* ---------------------------- */
+/* Panorama                     */
+/* ---------------------------- */
+
 
 // Keybinding for desktop
 $(document).on("keydown", function (e) {
